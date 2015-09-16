e166d7298b07bf25375af1c59a39d8fc6f6da84c
fab-du <batlab.official@gmail.com>
2015-09-16 20:37:35 +0200

    minor

--- zsh/lib/path.zsh
+++ zsh/lib/path.zsh
@@ -35,8 +35,6 @@ export EDITOR=vim
 export ANDROID_PRODUCT_OUT=$HOME/batlab/Android
 export ANDROID_HOME=$HOME/adt-bundle/sdk/
 export QT_SELECT=5
-#export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:bd=40;33;01:cd=40;33;01:or=01;05;37;41:mi=01;05;37;41:ex=01;32:*.cmd=01;32:*.exe=01;32:*.com=01;32:*.btm=01;32:*.bat=01;32:*.sh=01;32:*.csh=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.bz=01;31:*.tz=01;31:*.rpm=01;31:*.cpio=01;31:*.jpg=01;35:*.gif=01;35:*.bmp=01;35:*.xbm=01;35:*.xpm=01;35:*.png=01;35:*.tif=01;35:'
-
 
 export FZF=/home/batie/Downloads/fzf-master/bin 
 export FZF PATH
@@ -49,7 +47,7 @@ export MPD_HOST PATH
 export NVM_DIR="/home/batie/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
-export NODE_DIR="/home/batie/.nvm/versions/node/4.0.0/bin"
+export NODE_DIR=/home/batie/.nvm/versions/node/v4.0.0/bin
 
 export NVM_DIR PATH
 export NODE_DIR PATH
