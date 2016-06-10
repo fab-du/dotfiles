d24d6006f218e83e5bb0761d02158bcf365b8b6f
fab-du ( batlab) <batlab.official@gmail.com>
2016-06-10 12:43:22 +0200

    Update vim.sh
    update

--- install/vim.sh
+++ install/vim.sh
@@ -3,7 +3,7 @@
 echo "################################"
 echo "## Download vim plugin ##"
 
-if [ ! -e "$HOME"/.vim/autoload/plug.vim ] then
+if [ ! -e "$HOME"/.vim/autoload/plug.vim ]; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
