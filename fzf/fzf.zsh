c37e440afe26cf83ba3673a7fa165f3c275bca22
fab-du <batlab.official@gmail.com>
2016-09-26 16:22:53 +0200

    add fzf config

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -1,44 +1,47 @@
-export FZF_DEFAULT_COMMAND='ag -l -g ""'
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+# Setup fzf
 
-# If you're running fzf in a large git repository, git ls-tree can boost up the speed of the traversal.
+# ENV
 
+#pass additional options to C-R (history)
+export FZF_CTRL_R_OPTS=""
 
+export FZF_ALT_C_COMMAND="" # to override the default command
+export FZF_ALT_C_OPTS=""    # to pass additional options
+
+# Use ~~ as the trigger sequence instead of the default **
+export FZF_COMPLETION_TRIGGER='~~'
+
+# Options to fzf command
+export FZF_COMPLETION_OPTS='+c -x'
+
+# ---------
+if [[ ! "$PATH" == */home/batie/.fzf/bin* ]]; then
+  export PATH="$PATH:$HOME/bin_external/fzf/bin"
+fi
+
+# Man path
+# --------
+if [[ ! "$MANPATH" == *$HOME/.fzf/man* && -d "$HOME/.fzf/man" ]]; then
+  export MANPATH="$MANPATH:$HOME/.fzf/man"
+fi
 
 # Auto-completion
 # ---------------
-# [[ $- =~ i ]] && source "/home/batie/bin_external/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-# source "/home/batie/bin_external/fzf/shell/key-bindings.zsh"
-
-# Further custumization
-#
-
-function detofr {
-    readonly DICT_PATH=~/Downloads/de_fr.txt
-    if [ -z "$1" ]; then
-        cat $DICT_PATH | fzf-tmux | xclip -i -selection clipboard
-    else
-        ag --no-number --ignore-case "$1" $DICT_PATH | fzf-tmux -q "$1" | xclip -i -selection clipboard
-    fi
-}
-
-function buff {
-    readonly BUFF=~/.buffer
-     cat ~/.buffer | fzf-tmux |  xclip -i
-}
-
-function gi() { curl -L -s https://www.gitignore.io/api/$@ ;}
+source "$HOME/.fzf/shell/key-bindings.zsh"
 
 # fe [FUZZY PATTERN] - Open the selected file with the default editor
 #   - Bypass fuzzy finder if there's only one match (--select-1)
 #   - Exit if there's no match (--exit-0)
 fe() {
-  local file
-  file=$(fzf --query="$1" --select-1 --exit-0)
-  [ -n "$file" ] && ${EDITOR:-vim} "$file"
+  IFS='
+'
+  local declare files=($(fzf-tmux --query="$1" --select-1 --exit-0))
+  [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
+  unset IFS
 }
 
 # Modified version where you can press
@@ -54,14 +57,61 @@ fo() {
   fi
 }
 
+# vf - fuzzy open with vim from anywhere
+# ex: vf word1 word2 ... (even part of a file name)
+# zsh autoload function
+vf() {
+  local files
+
+  files=(${(f)"$(locate -Ai -0 $@ | grep -z -vE '~$' | fzf --read0 -0 -1 -m)"})
+
+  if [[ -n $files ]]
+  then
+     vim -- $files
+     print -l $files[1]
+  fi
+}
+
 # fd - cd to selected directory
 fd() {
   local dir
-  dir=$(find ${1:-*} -path '*/\.*' -prune \
+  dir=$(find ${1:-.} -path '*/\.*' -prune \
                   -o -type d -print 2> /dev/null | fzf +m) &&
   cd "$dir"
 }
 
+# fdr - cd to selected parent directory
+fdr() {
+  local declare dirs=()
+  get_parent_dirs() {
+    if [[ -d "${1}" ]]; then dirs+=("$1"); else return; fi
+    if [[ "${1}" == '/' ]]; then
+      for _dir in "${dirs[@]}"; do echo $_dir; done
+    else
+      get_parent_dirs $(dirname "$1")
+    fi
+  }
+  local DIR=$(get_parent_dirs $(realpath "${1:-$(pwd)}") | fzf-tmux --tac)
+  cd "$DIR"
+}
+# cf - fuzzy cd from anywhere
+# ex: cf word1 word2 ... (even part of a file name)
+# zsh autoload function
+cf() {
+  local file
+
+  file="$(locate -Ai -0 $@ | grep -z -vE '~$' | fzf --read0 -0 -1)"
+
+  if [[ -n $file ]]
+  then
+     if [[ -d $file ]]
+     then
+        cd -- $file
+     else
+        cd -- ${file:h}
+     fi
+  fi
+}
 
 # cdf - cd into the directory of the selected file
 cdf() {
@@ -70,40 +120,10 @@ cdf() {
    file=$(fzf +m -q "$1") && dir=$(dirname "$file") && cd "$dir"
 }
 
-
 # fh - repeat history
 fh() {
   print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
 }
-
-# Another CTRL-R script to insert the selected command from history into the command line/region
-__fzf_history ()
-{
-    builtin history -a;
-    builtin history -c;
-    builtin history -r;
-    builtin typeset \
-        READLINE_LINE_NEW="$(
-            HISTTIMEFORMAT= builtin history |
-            command fzf +s --tac +m -n2..,.. --tiebreak=index --toggle-sort=ctrl-r |
-            command sed '
-                /^ *[0-9]/ {
-                    s/ *\([0-9]*\) .*/!\1/;
-                    b end;
-                };
-                d;
-                : end
-            '
-        )";
-    READLINE_LINE="${READLINE_LINE:+${READLINE_LINE:0:READLINE_POINT}}${READLINE_LINE_NEW}${READLINE_LINE:+${READLINE_LINE:READLINE_POINT}}";
-    READLINE_POINT="$((READLINE_POINT + ${#READLINE_LINE_NEW}))"
-}
-
-#builtin set -o histexpand;
-#builtin bind '"\er": redraw-current-line';
-#builtin bind '"\e^": magic-space';
-#builtin bind -x '"\C-x1": __fzf_history';
-#builtin bind '"\C-r": "\C-x1\e^\er"'
 
 # fkill - kill process
 fkill() {
@@ -113,6 +133,67 @@ fkill() {
   then
     kill -${1:-9} $pid
   fi
+}
+
+# fbr - checkout git branch
+fbr() {
+  local branches branch
+  branches=$(git branch -vv) &&
+  branch=$(echo "$branches" | fzf +m) &&
+  git checkout $(echo "$branch" | awk '{print $1}' | sed "s/.* //")
+}
+
+# fbr - checkout git branch (including remote branches)
+fbr() {
+  local branches branch
+  branches=$(git branch --all | grep -v HEAD) &&
+  branch=$(echo "$branches" |
+           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
+
+# fco - checkout git branch/tag
+fco() {
+  local tags branches target
+  tags=$(
+    git tag | awk '{print "\x1b[31;1mtag\x1b[m\t" $1}') || return
+  branches=$(
+    git branch --all | grep -v HEAD             |
+    sed "s/.* //"    | sed "s#remotes/[^/]*/##" |
+    sort -u          | awk '{print "\x1b[34;1mbranch\x1b[m\t" $1}') || return
+  target=$(
+    (echo "$tags"; echo "$branches") |
+    fzf-tmux -l30 -- --no-hscroll --ansi +m -d "\t" -n 2) || return
+  git checkout $(echo "$target" | awk '{print $2}')
+}
+
+# fcoc - checkout git commit
+fcoc() {
+  local commits commit
+  commits=$(git log --pretty=oneline --abbrev-commit --reverse) &&
+  commit=$(echo "$commits" | fzf --tac +s +m -e) &&
+  git checkout $(echo "$commit" | sed "s/ .*//")
+}
+
+# fshow - git commit browser
+fshow() {
+  git log --graph --color=always \
+      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+  fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+      --bind "ctrl-m:execute:
+                (grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                {}
+FZF-EOF"
+}
+
+# fcs - get git commit sha
+# example usage: git rebase -i `fcs`
+fcs() {
+  local commits commit
+  commits=$(git log --color=always --pretty=oneline --abbrev-commit --reverse) &&
+  commit=$(echo "$commits" | fzf --tac +s +m -e --ansi --reverse) &&
+  echo -n $(echo "$commit" | sed "s/ .*//")
 }
 
 # fs [FUZZY PATTERN] - Select selected tmux session
@@ -125,13 +206,16 @@ fs() {
   tmux switch-client -t "$session"
 }
 
-# ftpane - switch pane
-ftpane () {
-  local panes current_window target target_window target_pane
+# ftpane - switch pane (@george-b)
+# In tmux.conf
+# bind-key 0 run "tmux split-window -l 12 'bash -ci ftpane'"
+ftpane() {
+  local panes current_window current_pane target target_window target_pane
   panes=$(tmux list-panes -s -F '#I:#P - #{pane_current_path} #{pane_current_command}')
-  current_window=$(tmux display-message  -p '#I')
+  current_pane=$(tmux display-message -p '#I:#P')
+  current_window=$(tmux display-message -p '#I')
 
-  target=$(echo "$panes" | fzf) || return
+  target=$(echo "$panes" | grep -v "$current_pane" | fzf +m --reverse) || return
 
   target_window=$(echo $target | awk 'BEGIN{FS=":|-"} {print$1}')
   target_pane=$(echo $target | awk 'BEGIN{FS=":|-"} {print$2}' | cut -c 1)
